edbdbc343c6c1eb7aaf8fe89631d92c41ca1cba0
Darwhin Gomez <128323290+DW8888@users.noreply.github.com>
2024-03-18 09:20:08 -0400

    FIXED FOLDERS

---
+++ src/EMPLOYEE/PayRecord.java
@@ -0,0 +1,52 @@
+package CUSTOMER;
+
+public class PayRecord {
+    private String EmployeeID;
+    private double Salary;
+    private String Paydate;
+
+    public PayRecord() {
+    }  
+
+    public PayRecord(String EmployeeID, double Salary, String Paydate) {
+        this.EmployeeID = EmployeeID;
+        this.Salary = Salary;
+        this.Paydate = Paydate;
+    }
+
+    public String getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        EmployeeID = employeeID;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(double salary) {
+        Salary = salary;
+    }
+
+    public String getPaydate() {
+        return Paydate;
+    }
+
+    public void setPaydate(String paydate) {
+        Paydate = paydate;
+    }
+    public String toString() {
+        super.toString();
+        return "PayRecord \nEmployeeID=" + EmployeeID + 
+        "\n Salary=" + Salary + "\n Paydate=" + Paydate + "]";
+    }
+    public void displayMe() {
+        System.out.println(toString());
+    }
+
+
+    
+
+}
